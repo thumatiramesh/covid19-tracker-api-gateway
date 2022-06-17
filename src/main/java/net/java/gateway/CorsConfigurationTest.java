1127409cd32c3b3cdc697d974dbf59366cd60cf3
Ramesh <ramesh.thumati@odinmo.com>
2022-06-17 21:02:52 +0530

    intial working commit

---
+++ src/main/java/net/java/gateway/CorsConfigurationTest.java
@@ -0,0 +1,28 @@
+package net.java.gateway;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfigurationTest {
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+		corsConfig.addAllowedHeader("*");
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
+}
---
+++ src/main/java/net/java/gateway/SecurityConfig.java
@@ -0,0 +1,10 @@
+//package net.java.gateway;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//	protected void configure(HttpSecurity http) throws Exception{
+//		http.cors().and();
+//	}
+//}
